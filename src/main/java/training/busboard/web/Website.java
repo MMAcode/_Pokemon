503e8daf0f097d7d87665dc1562ee2d4bd9bee21
Hugo Javadi <hugo_jb@icloud.com>
2020-09-22 13:32:41 +0200

    Fetch Pokemon data from API

--- src/main/java/training/busboard/web/Website.java
+++ src/main/java/training/busboard/web/Website.java
@@ -1,5 +1,8 @@
 package training.busboard.web;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
@@ -7,7 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
@@ -19,13 +29,56 @@ public class Website {
     }
 
     @RequestMapping("/pokemonInfo")
-    ModelAndView pokemonInfo(@RequestParam("name") String name) {
-        ArrayList<String> abilities = new ArrayList<>();
-        abilities.add("limber");
-        abilities.add("imposter");
-        ArrayList<String> evolutions = new ArrayList<>();
-        evolutions.add("level-up");
-        return new ModelAndView("info", "pokemon", new Pokemon("Ditto", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png", abilities, evolutions));
+    ModelAndView pokemonInfo(@RequestParam("nameOrId") String nameOrId) {
+        JsonObject response = makeRequest("https://pokeapi.co/api/v2/pokemon/" + nameOrId);
+        if (response == null) return new ModelAndView("not-found", "", null);
+        String name = response.get("name").getAsString();
+        String pictureUrl = response.get("sprites").getAsJsonObject().get("front_default").getAsString();
+        List<String> abilities = new ArrayList<>();
+        response.get("abilities").getAsJsonArray().forEach(jsonElement -> abilities.add(jsonElement.getAsJsonObject().get("ability").getAsJsonObject().get("name").getAsString()));
+        response = makeRequest(response.get("species").getAsJsonObject().get("url").getAsString());
+        System.out.println(response);
+        response = makeRequest(response.get("evolution_chain").getAsJsonObject().get("url").getAsString());
+
+        List<String> evolutions = new ArrayList<>();
+
+        JsonObject node = response.getAsJsonObject("chain");
+
+        while (true) {
+            evolutions.add(node.get("species").getAsJsonObject().get("name").getAsString());
+            JsonArray evolvesTo = node.getAsJsonArray("evolves_to");
+            if (evolvesTo.size() == 0) break;
+            node = evolvesTo.get(0).getAsJsonObject();
+        }
+
+        return new ModelAndView("info", "pokemon", new Pokemon(name, pictureUrl, abilities, evolutions));
+    }
+
+    private JsonObject makeRequest(String url) {
+        try {
+            HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "");
+            BufferedReader in;
+            try {
+                in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } catch (Exception e) {
+                return null;
+            }
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
+            return new JsonParser().parse(String.valueOf(content)).getAsJsonObject();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void main(String[] args) {
